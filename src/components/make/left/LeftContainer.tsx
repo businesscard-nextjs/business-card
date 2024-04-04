682b48b558deff67b0f128648583ef089e62fbcd
yoodeve <janhold@naver.com>
2024-04-04 10:48:49 +0900

    feat: 왼쪽 컴포넌트 가운데 정렬 수정

--- src/components/make/left/LeftContainer.tsx
+++ src/components/make/left/LeftContainer.tsx
@@ -6,7 +6,7 @@ import ElementInput from "./ElementInput";
 
 function LeftContainer() {
   return (
-    <div className="w-1/2 min-h-screen bg-[#FDF2DD] flex flex-col">
+    <div className="w-1/2 min-h-screen bg-[#FDF2DD] flex flex-col justify-center items-center">
       <Header />
       <SizeDecideBox />
       <ElementPicker />
