11392b4eeb45670aeb69ced904a7ab3763e93a04
jeongwon <jungwon050897@gmail.com>
2024-04-08 14:17:45 +0900

    💄 # 404 페이지 퍼블리싱

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NotFound() {
-  return <div>Error Page: custom</div>;
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-white">
+      <Image
+        src="/images/not_found.svg"
+        alt="Not Found"
+        width={600}
+        height={0}
+        priority
+        className="mr-10"
+      />
+      <Link
+        href={"/"}
+        className="button large active hover w-[240px] font-semibold"
+      >
+        Go Home
+      </Link>
+    </div>
+  );
 }
